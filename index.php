9bc633f9cafde2021a7e9008224d466cbd7838fc
JohnAlex <bartho1010@gmail.com>
2019-02-14 10:03:54 -0500

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- METAS -->
+	<meta charset="UTF-8" />
+	<title>TodoList | John Alex</title>
+	<meta http-equiv="X-UA-Compatible" content="EDGE" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />	
+	<meta name="description" content=""/>
+	<meta name="keywords" content="">
+	<meta name="author" content="John Alex Fandiño">
+
+	<!-- LINK -->
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="css/main-min.css" />
+
+</head>
+<body>
+
+
+	<script src="js/main-min.js"></script>
+</body>
+</html>
